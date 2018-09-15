0d311dbf8bef1875f2063f628787978a33228522
Jared <kuil5230@gmail.com>
2018-09-15 18:38:03 +0800

    first commit

---
+++ mobile01-Crawler/README.md
@@ -0,0 +1,12 @@
+# mobile01 爬蟲
+
+### 使用 Node.js
+
+### 執行函數
+
+    StartCrawler(forum, startCode, total, output);
+
+    forum = 討論區代號
+    startCode = 從第幾頁開始爬
+    total = 該討論區總共頁數
+    output = 輸出檔案路徑(欲寫入的json需預設有一個Array)
---
+++ mobile01-Crawler/index.js
@@ -0,0 +1,84 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+//寫入檔案
+import { exportResults } from "./src/exportResult";
+//延遲執行
+import { waitFor, asyncForEach } from "./src/delayFunction";
+
+const getWebSiteContent = async (url, coverFile) => {
+  const linkList = [];
+  //取得每一頁的文章url
+  const RequestLink = async () => {
+    try {
+      const ResponseHTML = await axios.get(url);
+      const $ = cheerio.load(ResponseHTML.data);
+      $(".subject > span > a").each((index, value) => {
+        linkList.push({ link: "https://www.mobile01.com/" + $(value).attr("href"), page: $(".numbers").text() });
+      });
+    } catch (error) {}
+  };
+
+  const result = [];
+  const RequestDataAsync = async (url, page) => {
+    try {
+      const ResponseHTML = await axios.get(url);
+      const $ = cheerio.load(ResponseHTML.data);
+
+      //主題回覆 list
+      const ReplyList = [];
+      $(".single-post-content").map((index, value) => {
+        ReplyList.push(
+          $(".single-post-content")
+            .eq(index + 1)
+            .text()
+            .replace(new RegExp("\\n|\\s+|{|}|\"|'", "g"), "")
+        );
+      });
+
+      result.push({
+        article_title: $("#forum-content-main > h1")
+          .first()
+          .text(),
+        content: $(".single-post-content")
+          .first()
+          .text()
+          .replace(new RegExp("\\n|\\s+|{|}|\"|'", "g"), ""),
+        messages: ReplyList,
+        page: page
+      });
+
+      console.log(page, new Date().toString());
+    } catch (error) {}
+    await exportResults(result, coverFile);
+  };
+
+  Promise.resolve()
+    .then(() => RequestLink())
+    .then(() =>
+      Promise.all(
+        linkList.map(async link => {
+          await RequestDataAsync(link.link, link.page);
+        })
+      )
+    );
+};
+
+// forum = 討論區代號
+// startCode = 從第幾頁開始爬
+// total = 該討論區總共頁數
+// output = 輸出路徑
+const StartCrawler = async (forum, startCode, totalCode, output) => {
+  const list = [];
+  for (let i = startCode; i < totalCode; i++) {
+    list.push(i);
+  }
+  await asyncForEach(list, async num => {
+    await waitFor(5000);
+    getWebSiteContent("https://www.mobile01.com/topiclist.php?f=" + forum + "&p=" + num, output);
+  });
+  console.log("Done");
+};
+
+StartCrawler(692, 1, 5, "./data/result.json");
+// 欲寫入的json需預設有一個Array
